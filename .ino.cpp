2255bc354bc13db1b075d6f94c295a3c2b3a37fd
Kris Kalavantavanich <krismath.K@gmail.com>
2017-04-12 11:09:20 +0700

    system update

--- .ino.cpp
+++ .ino.cpp
@@ -2,7 +2,7 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2017-04-12 11:01:27
+//This file has been generated on 2017-04-12 11:08:26
 
 #include "Arduino.h"
 #include "Arduino.h"
